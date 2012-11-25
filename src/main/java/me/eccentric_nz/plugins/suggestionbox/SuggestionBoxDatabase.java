283889b2ed151a75b5c3718fc9cfe93f2cc379ee
eccentric devotion <eccentricdevotion@home.nz>
2012-11-25 16:39:29 +1300

    Create a new method addDefault and only run it if first_run is true

--- src/main/java/me/eccentric_nz/plugins/suggestionbox/SuggestionBoxDatabase.java
+++ src/main/java/me/eccentric_nz/plugins/suggestionbox/SuggestionBoxDatabase.java
@@ -11,7 +11,6 @@ public class SuggestionBoxDatabase {
     private static SuggestionBoxDatabase instance = new SuggestionBoxDatabase();
     public Connection connection = null;
     public Statement statement;
-    private SuggestionBox plugin;
 
     public static synchronized SuggestionBoxDatabase getInstance() {
         return instance;
@@ -29,14 +28,19 @@ public class SuggestionBoxDatabase {
     public void createTables() {
         try {
             statement = connection.createStatement();
-            String querySuggestions = "CREATE TABLE IF NOT EXISTS suggestions (sb_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, suggestion TEXT, player TEXT, type TEXT DEFAULT 'Suggestion', priority INTEGER DEFAULT 0)";
+            String querySuggestions = "CREATE TABLE IF NOT EXISTS suggestions (sb_id INTEGER PRIMARY KEY NOT NULL, suggestion TEXT, player TEXT, type TEXT DEFAULT 'Suggestion', priority INTEGER DEFAULT 0)";
             statement.executeUpdate(querySuggestions);
-            ResultSet rsFirst = statement.executeQuery("SELECT * FROM suggestions");
-            if (!rsFirst.next()) {
-                String queryAddDefault = "INSERT INTO suggestions (suggestion, player, type, priority) VALUES ('Welcome to SuggestionBox!', 'eccentric_nz','Comment',3)";
-                statement.executeUpdate(queryAddDefault);
-            }
-            rsFirst.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(SuggestionBoxConstants.MY_PLUGIN_NAME + " Create table error: " + e);
+        }
+    }
+
+    public void addDefault() {
+        try {
+            statement = connection.createStatement();
+            String queryAddDefault = "INSERT INTO suggestions (suggestion, player, type, priority) VALUES ('Welcome to SuggestionBox!', 'eccentric_nz','Comment',3)";
+            statement.executeUpdate(queryAddDefault);
             statement.close();
         } catch (SQLException e) {
             System.err.println(SuggestionBoxConstants.MY_PLUGIN_NAME + " Create table error: " + e);
